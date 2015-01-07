e30800129e522d04b0bc2e01e729e145eebf0521
Les-Rae.Superman <superman.lesrae111@gmail.com>
2015-01-07 14:13:41 +0200

    suresh done it

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'My Property Book' });
+  res.render('index', { title: 'My property Book' });
 });
 
 router.get('*', function(req, res) {
