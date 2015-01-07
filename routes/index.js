29b1168446d5e64e69ea296532fe4f8217669868
Les-Rae.Superman <superman.lesrae111@gmail.com>
2015-01-07 14:10:44 +0200

    name change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'myPropertyBook' });
+  res.render('index', { title: 'My Property Book' });
 });
 
 router.get('*', function(req, res) {
