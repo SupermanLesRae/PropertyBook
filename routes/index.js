400ec1d208956741fdcef77ff13d559e4a742a84
Les-Rae.Superman <superman.lesrae111@gmail.com>
2015-01-13 17:03:49 +0200

    page additions

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,21 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'My property Book', brand: 'PropertyBook' });
 });
 
+/* GET profile find page. */
+router.get('/find', function(req, res) {
+  res.render('find', { title: 'Find Uses', brand: 'PropertyBook' });
+});
+
+/* GET profile page. */
+router.get('/profile', function(req, res) {
+  res.render('profile', { title: 'My Profile', brand: 'PropertyBook' });
+});
+
+/* GET profile/settings page. */
+router.get('/settings', function(req, res) {
+  res.render('settings', { title: 'Settings', brand: 'PropertyBook' });
+});
+
 router.get('*', function(req, res) {
   res.render('error', { title: 'Page Error 404' });
 });
